2024434344e30386c42a23c1c4fc1317423f75c3
João Vitor Batista <joaovtb00@gmail.com>
2026-06-06 22:56:15 -0300

    Implementando busca paginada de produtos

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -5,20 +5,27 @@ import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
-    @Autowired
-    private ProductService productService;
+    @Autowired // Injeção de dependência
+    private ProductService productService; //inversão de controle
 
-    @GetMapping(value = "/{id}")
-    public ProductDTO findById(@PathVariable Long id){
-        return productService.findById(id);
+    @GetMapping(value = "/{id}") // adiciona um parametro na URL: /products/1
+    public ProductDTO findById(@PathVariable Long id){ //PathVariable captura o id que veio na requisição e joga no argumento.
+        return productService.findById(id); /*chamada do Service que busca o produto no banco de dados e retorna um DTO para ser retornada pro consumidor da API*/
     }
 
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){ //Pageable
+        return productService.findAll(pageable);
+    }
 }
